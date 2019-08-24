9a22e1594c5f6e3ff91ab44085d4f059f6985fa7
anbreaker <antunez19@gmail.com>
2019-08-24 12:56:02 +0200

    agregando sumaTodosLosCuadrados

--- sumaTodos.py
+++ sumaTodos.py
@@ -9,4 +9,17 @@ def sumaTodos(limitTo):
 
 print(sumaTodos(100))
 
+def sumaTodosLosCuadrados(limitTo):
+    resultado = 0
+    
+    for i in range(0, limitTo + 1):
+        
+        resultado = resultado + i*i
+        
+    return resultado
+
+print(sumaTodosLosCuadrados(3))
+
+#Toca 2121
+
 
