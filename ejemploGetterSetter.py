a4ecde50bfa672d930b48efcccb98b8848672667
anbreaker <antunez19@gmail.com>
2019-09-04 20:27:09 +0200

    Ejemplo de GetterSetter

--- ejemploGetterSetter.py
+++ ejemploGetterSetter.py
@@ -1,9 +1,28 @@
 class ClaseConGetterSetter():
-    def def __init__(self):
+    def __init__(self):
       self.__propiedad_privada = None
     
+    #Getters
+    def getPropiedadPrivada(self):
+        return self.__propiedad_privada
+
+    #Setters
     def setPropiedadPrivada(self, valor):
         self.__propiedad_privada = valor
 
+    #Getters y Setters Juntos
+    def propiedadPrivada(self, valor = None):
+        #Actuacion del Getter
+        if valor == None:
+            return self.__propiedad_privada
+        #Actuacion del Setter
+        else:
+            self.__propiedad_privada = valor
+            
+    
     def __str__(self):
         return "ClaseConGetterSetter con propiedadPrivada -> {}".format(self.__propiedad_privada)
+    
+
+if __name__ == '__main__':
+    c = ClaseConGetterSetter()
