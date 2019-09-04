f607621ef9634336f889bbddfce240f12f586045
anbreaker <antunez19@gmail.com>
2019-09-04 10:47:14 +0200

    Objeto terminado

--- conversorTemperaturaObjetos/termometro.py
+++ conversorTemperaturaObjetos/termometro.py
@@ -7,12 +7,13 @@
 #  - temperatura    #
 #-------------------#
 #  - conversor(T,U) #
+#  - get y set      #
 #-------------------#
 
 class Termometro():
     def __init__(self):
-      self.unidad = 'C'
-      self.temperatura = 0
+      self.__unidad = 'C'
+      self.__temperatura = 0
 
     def conversor(self, temperatura, unidad):
         if unidad == 'C':
@@ -23,7 +24,28 @@ class Termometro():
             return 'Unidad incorrecta'
     
     def __str__(self):
-        return "{}º {}".format(self.temperatura, self.unidad)
+        return "{}º {}".format(self.__temperatura, self.__unidad)
+
+
+    def unidadMedida(self, unidadM = None):
+        if unidadM == None:
+            return self.__unidad
+        else:
+            if unidadM == 'F' or unidadM == 'C':
+                self.__unidad = unidadM
+    
+    def temp(self, temperatura = None):
+        if temperatura == None:
+            return self.__temperatura
+        else:
+            self.__temperatura = temperatura
+    
+    def mide(self, uniM=None):
+        if uniM == None:
+            return self.__str__()
+        else:
+            return self.conversor(self.__temperatura, self.__unidad)
+
 
 def main():
     c = Termometro()
@@ -31,3 +53,10 @@ def main():
     print(c.conversor(32, 'F'))
     
 main()
+
+''' Pruebas
+t = Termometro()
+t.temp(32)
+t.unidadMedida('F')
+t.mide()
+'''
